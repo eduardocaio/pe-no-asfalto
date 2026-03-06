af9a1772f1d14210e36cbcd225bd794edf2f6f50
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2026-03-05 22:52:45 -0300

    refactor: rename menu activity and add new fields to refueling entity

--- app/src/main/java/com/eduardocaio/penoasfalto/data/model/Refueling.java
+++ app/src/main/java/com/eduardocaio/penoasfalto/data/model/Refueling.java
@@ -1,4 +1,24 @@
 package com.eduardocaio.penoasfalto.data.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.math.BigDecimal;
+
+@Entity(tableName = "refueling")
 public class Refueling {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    @ColumnInfo(name = "refuel_date_time")
+    public long refuelDateTime;
+
+    @ColumnInfo(name = "odometer_at_reserve")
+    public int odometerAtReserve;
+
+    @ColumnInfo(name = "amount_paid")
+    public BigDecimal amountPaid;
+
 }
