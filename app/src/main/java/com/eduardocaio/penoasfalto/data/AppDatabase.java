d30259a45e5bd1dd3444ac0911be98ecdb5cc890
Caio Eduardo <caiopessoal.tecnico@outlook.com>
2026-03-05 23:33:01 -0300

    feat: create database instance

--- app/src/main/java/com/eduardocaio/penoasfalto/data/AppDatabase.java
+++ app/src/main/java/com/eduardocaio/penoasfalto/data/AppDatabase.java
@@ -1,4 +1,18 @@
 package com.eduardocaio.penoasfalto.data;
 
-public class AppDatabase {
+@Database(entities = {Refueling.class, RefuelingItem.class}, version = 1)
+@TypeConverters(Converters.class)
+public abstract class AppDatabase {
+    private static AppDatabase database;
+
+    public static synchronized AppDatabase getDatabase(Context context) {
+        if (database == null) {
+            database = Room.databaseBuilder(
+                context.getApplicationContext(),
+                AppDatabase.class,
+                "penoasfalto_db"
+            ).build();
+        }
+        return database;
+    }
 }
